91116cc59e976a0c590373edc22f182e9ae87247
%username% <%email%>
2024-04-17 10:12:19 -0400

    I updated act() method in Target class

--- target.java
+++ target.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class target here.
  * 
@@ -14,6 +14,23 @@ public class target extends Actor
      */
     public void act()
     {
-        //stuff lol.
+        List<CannonBall> balls = getWorld().getObjects(CannonBall.class);
+        
+        for (int i=0; i < balls.size(); i++)
+        {
+            CannonBall ball = balls.get(i);
+            
+            Vector2D targetToBall = new Vector2D(ball.getX() - getX(), ball.getY() - getY());
+            double distance = targetToBall.magnitude();
+            
+            double ballRadius = ball.getImage().getHeight()/2;
+            double targetRadius = getImage().getHeight()/2;
+            
+            if (distance < ballRadius + targetRadius)
+            {
+                setImage("targetDestroyed.png");
+                Greenfoot.playSound("explosion.wav");
+            }
+        }
     }
 }
