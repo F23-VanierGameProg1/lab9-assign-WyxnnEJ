c7630e2a61371a53edefc4a29b9782dc79051dcd
%username% <%email%>
2024-04-17 10:05:08 -0400

    I added cannon rotation and shoot balls in Cannon class

--- MyWorld.java
+++ MyWorld.java
@@ -10,13 +10,14 @@ public class MyWorld extends World
 {
     private long lastFrameTimeMS;
     private double timeStepDuration;
-    
+
     public MyWorld()
     {    
         super(768, 576, 1, false);
-        
+
         lastFrameTimeMS = System.currentTimeMillis();
         timeStepDuration = 1.0 / 60; // seems to be the default
+        prepare();
     }
 
     public void started()
@@ -29,10 +30,27 @@ public class MyWorld extends World
         timeStepDuration = (System.currentTimeMillis() - lastFrameTimeMS) / 1000.0;
         lastFrameTimeMS = System.currentTimeMillis();
     }
-    
+
     public double getTimeStepDuration()
     {
         return timeStepDuration;
     }
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        target target = new target();
+        addObject(target,294,205);
+        target target2 = new target();
+        addObject(target2,484,240);
+        target target3 = new target();
+        addObject(target3,553,372);
+        target target4 = new target();
+        addObject(target4,660,529);
+        cannon cannon = new cannon();
+        addObject(cannon,83,504);
+    }
 }
